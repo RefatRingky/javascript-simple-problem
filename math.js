5f8fa0668d53b97b83817c883585debd400af1d1
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-01 12:28:43 +0600

    second commit

--- math.js
+++ math.js
@@ -1,3 +1,14 @@
-const myNumber = -5;
-const output = Math.abs(myNumber);
+// const myNumber = -5;
+// const output = Math.abs(myNumber);
+// console.log(output);
+
+// const myNumber = 21.6666;
+// const output = Math.ceil(myNumber);
+// console.log(output);
+// const myNumber = 21.6666;
+// const output = Math.floor(myNumber);
+// console.log(output);
+
+const myNumber = 21.6666;
+const output = Math.round(myNumber);
 console.log(output);
